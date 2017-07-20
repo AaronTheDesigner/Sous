dc06b2bc8997b809a87cef12a347f4eb61ea4b94
Terri Pannell <Terri Pannell>
2017-07-20 15:18:07 -0400

    added weather call after geolocation call based on Jim's code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,36 @@
 //Global variables
 var pos = {lat: 0, long: 0};
+var bHavePos = false;
+var weather = {temp: 0, rain: false, snow: false, humidity: 0, desc: ""};
 
 
 //Click handlers
+//window.onload = function () {
+function getLatLong(){
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
+
+                bHavePos = true;
+              }, function() {
+
+                 $("#geoModal").modal();
+              },{timeout:5000}
+            );
+        } else {
+
+            $("#geoModal").modal();
+        } 
+
+        getWeather();
+
+    };
+
+
 
 $(document).on("click", "#submitAddressBtn", function(event){
       var zipcode ;//= prompt("Enter zip code");
@@ -43,6 +71,7 @@ $(document).on("click", "#submitAddressBtn", function(event){
         if(bValid){
            $("#geoModal").modal();
       }
+      getWeather();
 });
 
 //Functions
@@ -63,4 +92,45 @@ function getLatLong(){
 
         $("#geoModal").modal();
     } 
-};
+}
+
+function getWeather(){
+
+    if(bHavePos){
+        //pos.lat = 35.34558848;
+        //pos.lng = -80.7119484;
+        if(pos.lat != 0 && pos.lng != 0){
+
+            var queryURL = "http://api.openweathermap.org/data/2.5/find?lat="+pos.lat+"&lon="+pos.lng+"&units=imperial&appid=166a433c57516f51dfab1f7edaed8413";
+
+               // Creates AJAX call for the specific lon and lat of a city
+
+               $.ajax({
+                  url: queryURL,
+                  method: "GET"
+                }).done(function(response) {
+                  console.log(response);
+                  console.log(response.list[0].weather[0]);
+
+               weather.temp = response.list[0].main.temp;
+               weather.rain = response.list[0].rain;
+               weather.snow = response.list[0].snow;
+               weather.humidity = response.list[0].main.humidity;
+               weather.desc = response.list[0].weather[0].main;
+               console.log(weather);            
+                });
+            }
+                
+        } else {
+            //Don't want to check more than about 20 times
+            var counter = 0;
+            if(counter < 20){
+                counter++;
+
+                setTimeout(getWeather, 250); 
+               
+            }
+
+        }       
+    
+}
